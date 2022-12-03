4a571a0dea5322dc5dec60338210f958051d3e7b
unknown <kumarabhishek0947@gmail.comgit config --global user.name AAbhishek18>
2022-12-03 22:14:21 +0530

    updeted

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -2,24 +2,8 @@
 const jwt = require("jsonwebtoken");
 const blogModel = require("../models/blogModel");
 
-// //Authentication
-// const authEntication = async function (req, res, next) {
-//     try {
-//         let header = req.headers;
-//         let token = header["x-api-key"]
-//         if (!token) return res.status(400).send({ status: false, msg: "Sorry,Header(token) Must Needed" })
-//         let decodedtoken = jwt.verify(token, "group-25");
-//         console.log(decodedtoken)
-//         let authorLoged = decodedtoken.authorId;
-//         console.log(authorLoged)
-//         if (!authorLoged) return res.status(401).send({ status: false, msg: 'Invalid token' })
-//         next()
-//     }
-//     catch (error) {
-//         return res.status(500).send({ status: false, msg: error.message })
-//     }
-// }
-// Autherisation
+
+// Authorization middleware
 const authorIsation = async function (req, res, next) {
     try {
         let authorId;
@@ -28,10 +12,10 @@ const authorIsation = async function (req, res, next) {
         
         let decodedToken = jwt.verify(token, "abhishekKumar");
         
-       
-         authorId = decodedToken.authorId;//authorId is authorId of author who is loged in
+         //authorId is authorId of author who is loged in
+         authorId = decodedToken.authorId;
         
-         //fectching authorId from body or params
+         //fetching authorId from body or params
         let authorIdFromBody = req.body.authorId;
         let authorIdFromParams = req.query.authorId;
 
